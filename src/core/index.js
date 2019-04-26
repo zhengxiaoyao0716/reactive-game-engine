bcae235d5ef2054abcbac466f0879030cfdb5af6
zhengxiaoyao0716 <1499383852@qq.com>
2019-04-27 04:17:37 +0800

    修复在core/main里没有取到core/mock

--- src/core/index.js
+++ src/core/index.js
@@ -4,6 +4,8 @@ import { $Types } from '../util/@types';
 import { Subject } from 'rxjs';
 import { Data, mock } from './mock';
 
+export * from './mock';
+
 export const subject: Subject<Data> = new Subject();
 
 if (typeof window !== 'undefined' && window.core instanceof Function) window.core.subject = subject;
