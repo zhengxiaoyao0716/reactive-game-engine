43c41a6707f39db28b7f83270c86f9add939a10f
zhengxiaoyao0716 <1499383852@qq.com>
2019-04-26 21:41:49 +0800

    add scaleMode support for UI component

--- src/pixi/UI/index.js
+++ src/pixi/UI/index.js
@@ -1,25 +1,37 @@
-import React, { ReactNode, useMemo } from 'react';
+import React, { ReactNode, useMemo, CSSProperties } from 'react';
 import './index.css';
 import { useRenderer } from '../Renderer';
 import { useObservable } from '../../util/rxjs';
 import { fromEvent } from 'rxjs';
 import { map, debounceTime } from 'rxjs/operators';
+import { CanvasRenderer, WebGLRenderer } from 'pixi.js';
 
 interface Props {
     id?: string, className?: string;
     children?: ReactNode;
+    /** adapt the canvas by scaling the size instead of cropping it. */
+    scaleMode?: boolean,
 }
 
-const styleOf = ({ clientWidth: width, clientHeight: height, offsetLeft: left, offsetTop: top }: HTMLCanvasElement) => ({ width, height, left, top });
+const styleOf = (renderer: WebGLRenderer | CanvasRenderer, scaleMode: boolean): CSSProperties => {
+    const { clientWidth, clientHeight, offsetLeft, offsetTop } = renderer.view;
+    if (!scaleMode) return { width: clientWidth, height: clientHeight, left: offsetLeft, top: offsetTop };
+    const { width, height } = renderer;
+    return {
+        width, height,
+        transform: `scale(${clientWidth / width}, ${clientHeight / height})`,
+        left: (clientWidth - width) / 2 + offsetLeft, top: (clientHeight - height) / 2 + offsetTop,
+    };
+};
 
-export const UI = ({ id, className = '', children }: Props) => {
+export const UI = ({ id, className = '', children, scaleMode }: Props) => {
     const renderer = useRenderer();
     const styleEvent = useMemo(() =>
         fromEvent(window, 'resize')
-            .pipe(map(_event => styleOf(renderer.view)))
+            .pipe(map(_event => styleOf(renderer, scaleMode)))
             .pipe(debounceTime(100))
         , []);
-    const style = useObservable(styleEvent, styleOf(renderer.view));
+    const style = useObservable(styleEvent, styleOf(renderer, scaleMode));
     return (
         <div id={id} className={`${className && `${className} `}PIXI UI`} style={style}>{children}</div>
     );
