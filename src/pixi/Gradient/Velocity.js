296212601f6183949d318ddd46fb39fd9da0d2b9
zhengxiaoyao0716 <1499383852@qq.com>
2019-04-28 15:42:35 +0800

    Gradient.Velocity现在支持任意维度的向量叠加了

--- src/pixi/Gradient/Velocity.js
+++ src/pixi/Gradient/Velocity.js
@@ -1,24 +1,21 @@
 import { useCallback } from 'react';
 import { PointsFn, GradientFn } from '.';
 
-type State = {
-    position: number[],
-    velocity?: number[],
-    gravity?: number[],
-};
+type State = number[][];
 
 const velocityGradientFn = (): GradientFn<State, State> => (
     (ticker, [point], state) => {
         if (state == null) return point;
         const { deltaTime } = ticker;
 
-        const position = [...state.position];
-        const velocity = state.velocity ? [...state.velocity] : null;
-        const gravity = state.gravity || null;
+        const vectors = state.map(vector => [...vector]); // deep clone
+        for (let index = 1; index < vectors.length; index++) {
+            const vector = vectors[vectors.length - index];
+            const previous = vectors[vectors.length - index - 1];
+            vector.forEach((value, index) => previous[index] = previous[index] + value * deltaTime);
 
-        gravity && gravity.forEach((value, index) => velocity[index] += value * deltaTime);
-        velocity && velocity.forEach((value, index) => position[index] += value * deltaTime);
-        return { position, velocity, gravity };
+        }
+        return vectors;
     }
 );
 
