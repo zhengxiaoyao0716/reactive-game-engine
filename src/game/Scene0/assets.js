155b64805c2ff26a8579e69bbd3495216abd1317
zhengxiaoyao0716 <1499383852@qq.com>
2019-04-28 15:19:52 +0800

    修复scene0的example在移动端不等宽

--- src/game/Scene0/assets.js
+++ src/game/Scene0/assets.js
@@ -3,14 +3,14 @@ import { loaders } from 'pixi.js';
 
 const asset: UseResourceOptions = { // mock resource
     ...[
-        ' ■ \n■■■\n■ ■', ' ■ \n■ ■\n■ ■', // 00, 01: idle
-        '   \n■■■\n■ ■', ' ■ \n■ ■\n■ ■', // 02, 03: up
-        '■ ■\n■■■\n   ', '■ ■\n■ ■\n ■ ', // 04, 05: down
-        ' ■■\n ■ \n ■■', ' ■■\n■  \n ■■', // 06, 07: left
-        '■■ \n ■ \n■■ ', '■■ \n  ■\n■■ ', // 08, 09: right
-        ' ■ \n■ ■\n ■ ', '■ ■\n ■ \n■ ■', // 10, 11: boom
+        '　█　\n███\n█　█', '　█　\n█　█\n█　█', // 00, 01: idle
+        '　　　\n███\n█　█', '　█　\n█　█\n█　█', // 02, 03: up
+        '█　█\n███\n　　　', '█　█\n█　█\n　█　', // 04, 05: down
+        '　██\n　█　\n　██', '　██\n█　 \n　██', // 06, 07: left
+        '██　\n　█　\n██　', '██　\n 　█\n██　', // 08, 09: right
+        '　█　\n█　█\n　█　', '█　█\n　█　\n█　█', // 10, 11: boom
     ].map(
-        value => async make => make.text(value, { fontFamily: ['Consolas'], lineHeight: 16, fontSize: 24 })
+        value => async make => make.text(value, { fontFamily: ['monospace'], fontSize: 24 })
     ).reduce(
         (dict, item, index) => ({ ...dict, [`player${`0${index}`.slice(-2)}`]: item }), {}
     ),
