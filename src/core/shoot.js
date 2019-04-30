a158c78501d4d40c37263907ced3da4e7da99d20
zhengxiaoyao0716 <1499383852@qq.com>
2019-04-30 23:51:31 +0800

    飞机轨迹完成

--- src/core/shoot.js
+++ src/core/shoot.js
@@ -18,33 +18,33 @@ export const calcPosition = (boat: number, seat: number) => {
 };
 
 export type SeatedMember = {
+    index: number,
     position: [number, number],
 } & Member;
 
 export type MovingMember = {
     vertex: [[number, number], number, number], // [[x, y], angle, progress]
     velocity: [number, number, number],         // [线速度, 角速度, 匀速度]
-} & Member; // { position: [x, y, scale] }
+} & SeatedMember; // { position: [x, y, scale] }
 
 /**
  * 从分组集合中随机发射队员.
  * @param {ReturnType<typeof divideGroup>} groups 队员分组.
  * @param {number} freq 发射频率，多少秒发射一个.
  * @param {number} turn 转向次数，队员路径顶点数.
- * @param {number} bend 偏转角度
  */
-export const shootMember = (groups: ReturnType<typeof divideGroup>, freq = 1.5, turn = 3, bend = 1 / 3) => {
+export const shootMember = (groups: ReturnType<typeof divideGroup>, freq = 1.2, turn = 3) => {
     const seated: Subject<SeatedMember> = new Subject()
         .pipe(delay(turn * freq * 1000));
     const moving: Subject<MovingMember[]> = new Subject()
-        .pipe(scan(shootPathFit(freq, turn, bend), []));
+        .pipe(scan(shootPathFit(freq, turn), []));
 
     const subscription = from([
         ...shuffle(groups
-            .map((group, boat) => group.map(({ type: _, ...member }, seat): SeatedMember => ({ ...member, position: calcPosition(boat, seat) })))
+            .map((group, boat) => group.map(({ type: _, ...member }, seat) => ({ ...member, position: calcPosition(boat, seat) })))
             .reduce((members, group) => [...members, ...group], [])
-        ),
-        ...new Array(1 + turn).fill(null), // 最后来几个空数据占位
+        ).map((member, index): SeatedMember => ({ ...member, index })), // .slice(7, 8),
+        ...new Array(1 + turn).fill().map((): SeatedMember => null), // 最后来几个空数据占位
     ])
         .pipe(zip(interval(freq * 1000), member => member))
         .subscribe(member => {
@@ -55,58 +55,180 @@ export const shootMember = (groups: ReturnType<typeof divideGroup>, freq = 1.5, 
     return { seated, moving, subscription };
 };
 
-const shootPathFit = (freq: number, turn: number, bend: number) => {
+const shootPathFit = (freq: number, turn: number) => {
     const frames = 60 * freq;
-    const framesTotal = frames * turn;
     return (history: MovingMember[], member: SeatedMember) => (
         [member, ...history.slice(0, turn)].map(
             (member, index): MovingMember => {
                 if (member == null) return null;
-                const { name, position } = member;      // 目标坐标
-                const origin = shootOrigin(position);   // 起点坐标
-                const even = index % 2 === 0;           // 奇偶次数
-                const points = [[position[0], origin[0]], [position[1], origin[1]]];
-                const vector = points.map(([pos, org]) => pos - org);   // 方向
-                const offset = (even ? 1 : -1) * Math.PI * bend;        // 偏转
-                const offsetHalf = offset / 2;                          // 半偏转
 
-                // 计算折点
-                const progress = index / turn;                          // 当前进度
-                const angle = vectorAngle(vector) - offsetHalf;         // 角度
-                const point = points.map(([pos, org]) => org + progress * (pos - org));
+                const { name, index: id, position: target } = member;
+                const [originZero, fitFunction] = id % 8 === 3 ? [5, fitTwine] : id % 8 === 7 ? [3, fitSpiral] : [0, fitCircle];
+                const origin = shootOrigin(target, originZero);
 
-                // 计算速度
-                const ω = offset / frames;                                                                      // 角速度
-                const Δ = Math.sqrt(vector.map(vec => Math.pow(vec, 2)).reduce((xp, yp) => xp + yp)) / turn;    // 路程
-                const ν = Δ / Math.sin(offsetHalf) / 2;                                                         // 线速度 // 准确的说是线速度与角速度之比
+                const vector = [target[0] - origin[0], target[1] - origin[1]];                                  // 位移矢量
+                const distance = Math.sqrt(vector.map(vec => Math.pow(vec, 2)).reduce((xp, yp) => xp + yp));    // 位移标量
+                const angle = vectorAngle(vector);  // 方向角度
+                const progress = index / turn;      // 当前进度
+
+                const { vertex, velocity } = fitFunction({
+                    freq, turn, index, frames,
+                    target, origin, vector,
+                    distance, angle, progress,
+                });
 
                 return {
+                    ...member,
                     name: index < turn ? name : null,
-                    position,
-                    vertex: [point, angle, progress],
-                    velocity: [ν, ω, 1 / framesTotal],
+                    vertex: [...vertex, progress],
+                    velocity: [...velocity, 1 / frames / turn],
                 };
             }
         )
     );
 };
 
-const shootOrigin = (destination: [number, number], width = 1920, height = 1080) => {
-    const x = destination[0] - width / 2;
-    const y = height / 2 - destination[1];
-    return (y > 0
-        ? (x > 0
-            ? [0, height]       // 0
-            : [width, height]   // 1
-        )
-        : (x < 0
-            ? [width, 0]        // 2
-            : [0, 0]            // 3
-        )
+/**
+ * 0-------1------2>
+ * | \     |    / '
+ * |   \ 5 | 6 /  '
+ * |   4 \ | / 7  '
+ * 7--------------3
+ * |   3 / | \ 0  '
+ * |   / 2 | 1 \  '
+ * | /     |     \'
+ * 6       5      4
+ * v
+ */
+const shootOrigin = ([x, y]: [number, number], zero = 0, width = 1920, height = 1080) => {
+
+    const a = y - height / 2 > 0;           // [3, 4, 6, 7]:    y > 0
+    const b = y / height - x / width > 0;   // [4, 6, 0]:       y - x > 0
+    const c = x - width / 2 < 0;            // [5, 6, 0, 1]:    x < 0
+    const d = y / height + x / width < 1;   // [6, 0, 2]:       y + x < 1
+
+    const index = (
+        a && !b ? (0)
+            : b && !c ? (1)
+                : c && !d ? (2)
+                    : d && a ? (3)
+                        : !a && b ? (4)
+                            : !b && c ? (5)
+                                : !c && d ? (6)
+                                    : !d && !a ? (7) : 'never'
     );
+
+    return [
+        [0, 0],
+        [width / 2, 0],
+        [width, 0],
+        [width, height / 2],
+        [width, height],
+        [width / 2, height],
+        [0, height],
+        [0, height / 2],
+    ][(index + zero) % 8];
 };
 
 const vectorAngle = (vector: [number, number]) => {
     const angle = Math.atan(vector[1] / vector[0]);
     return (angle < 0 ? Math.PI + angle : angle) + (vector[1] < 0 ? Math.PI : 0);
+};
+
+// 路径拟合参数
+interface PathFitParam {
+    freq: number, turn: number, index: number, frames: number, // 基础参数
+    target: [number, number],   // 终点坐标
+    origin: [number, number],   // 起点坐标
+    vector: [number, number],   // 位移矢量
+    distance: number,           // 位移标量
+    angle: number,              // 方向角度
+    progress: number,           // 当前进度
+}
+
+// 相接等圆拟合
+const fitCircle = ({ turn, index, frames, origin, vector, distance, angle, progress }: PathFitParam, bend = 1 / 3) => {
+    const offset = (index % 2 === 0 ? 1 : -1) * Math.PI * bend; // 偏转
+    const offsetHalf = offset / 2;                              // 半偏转
+    const move = distance / turn;                               // 位移
+
+    // 当前位置
+    const point = [origin[0] + vector[0] * progress, origin[1] + vector[1] * progress];
+    // 旋转角度
+    const rotate = angle - offsetHalf;
+
+    // 计算速度
+    const ω = offset / frames;                  // 角速度
+    const ν = move / Math.sin(offsetHalf) / 2;  // 线速度 // 准确的说是线速度与角速度之比
+
+    return {
+        vertex: [point, rotate],
+        velocity: [ν, ω],
+    };
+};
+
+// 螺旋线形拟合
+const fitSpiral = ({ turn, index, frames, origin, vector, distance, angle }: PathFitParam) => {
+    const offset = Math.PI;         // 偏转
+    const offsetHalf = offset / 2;  // 半偏转
+
+    // 计算某点的向量相对第一次的倍率，[ratio, diameter]
+    const vectorRatio = (index: number): [number, number] => {
+        if (index === 0) return [0, 0];
+        if (index === 1) return [1, 1];
+        const [ratio, previos] = vectorRatio(index - 1);
+        // const diameter = previos * (turn - bend) / turn;
+        const diameter = previos - 1 / (turn + 1);
+        const odd = index % 2 === 1;
+        return [ratio + (odd ? 1 : -1) * diameter, diameter];
+    };
+    const ratioTarget = vectorRatio(turn);                          // 目标点的倍率
+    const vectorFirst = vector.map(vec => vec / ratioTarget[0]);    // 第一次的位移向量
+    const ratioLast = vectorRatio(index);                           // 上一次的倍率
+    const vectorLast = vectorFirst.map(vec => vec * ratioLast[0]);  // 上一次的位移向量
+    const ratioNow = vectorRatio(index + 1);                        // 这一次的倍率
+    const diameter = distance / ratioTarget[0] * ratioNow[1];       // 位移直径
+
+    // 当前位置
+    const point = [origin[0] + vectorLast[0], origin[1] + vectorLast[1]];
+    // 旋转角度
+    const rotate = angle - offsetHalf + (index % 2 === 0 ? 0 : Math.PI);
+
+    // 计算速度
+    const ω = offset / frames;  // 角速度
+    const ν = diameter / 2;     // 线速度 // 准确的说是线速度与角速度之比
+
+    return {
+        vertex: [point, rotate],
+        velocity: [ν, ω],
+    };
+};
+
+// 圆环缠绕拟合
+const fitTwine = ({ turn, index, frames, origin, vector, distance, angle }: PathFitParam, bend = 1 / 3) => {
+    const even = index % 2 === 0;                       // 奇偶次序
+    const offset = Math.PI * (even ? bend : 2 - bend);  // 偏转
+    const offsetHalf = offset / 2;                      // 半偏转
+
+    const knotsTotal = Math.ceil(turn / 2); // 总扭结数
+    const knotsNow = Math.ceil(index / 2);  // 当前扭结数
+
+    const ratioEven = (2 - bend) * 1 / (2 * knotsTotal * (1 - bend) + bend);            // 偶数弦长倍率
+    const ratioOdd = bend / (2 * knotsTotal * (1 - bend) + bend);                       // 奇数弦长倍率
+    const ratio = knotsNow * ratioEven - (even ? knotsNow : knotsNow - 1) * ratioOdd;   // 当前位置倍率
+    const delta = (even ? ratioEven : ratioOdd) * distance;                             // 当前弦长位移
+
+    // 当前位置
+    const point = [origin[0] + vector[0] * ratio, origin[1] + vector[1] * ratio];
+    // 旋转角度
+    const rotate = angle - offsetHalf - (even ? 0 : Math.PI);
+
+    // 计算速度
+    const ω = offset / frames;                  // 角速度
+    const ν = delta / Math.sin(offsetHalf) / 2; // 线速度 // 准确的说是线速度与角速度之比
+
+    return {
+        vertex: [point, rotate],
+        velocity: [ν, ω],
+    };
 };
